e4df7453a5f2e4987854417e017458480338192f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-12-27 15:49:04 +0900

    update winston logger

--- libs/winston.ts
+++ libs/winston.ts
@@ -1,7 +1,16 @@
 import winston from "winston";
 
+// https://zenn.dev/ropitaru/scraps/4d49afd30b1eaa
+// severity: <Log Lebel>を出力するJSONに含める、もしくはstderrを使うように設定する
+
+// stderrを使うのが楽だが、warnを扱うのであればseverityの方がよい。
+
 export const winstonLogger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  transports: [new winston.transports.Console({ stderrLevels: ["error"] })],
 });
